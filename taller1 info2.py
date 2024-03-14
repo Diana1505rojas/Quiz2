0314c7310ea43de61fc7901c12f913ac8658a0b2
mateorh22 <mateo.restrepo5@udea.edu.co>
2024-03-13 20:39:45 -0500

    Clase de implantes medicos

--- taller1 info2.py
+++ taller1 info2.py
@@ -1,2 +1,41 @@
-print("inicio")
-print("hhoi")
+from datetime import datetime
+# se creo la clase implante medico con los atributos generales que tendran todos los implantes
+class ImplanteMedico:
+    def __init__(self, tipo, material, tamaño):
+        self.__tipo = tipo
+        self.__material = material
+        self.__tamaño = tamaño
+        self.__fecha_implantacion = None
+        self.__medico_responsable = None
+        self.__estado = None
+
+    def __str__(self):
+        return f"Tipo: {self.__tipo}\nMaterial: {self.__material}\nTamaño: {self.__tamaño}\nFecha de Implante: {self.__fecha_implantacion}\nMédico Responsable: {self.__medico_responsable}\nEstado: {self.__estado}"
+#   se sobreescribe el metodo str para imprimir en pantalla un objeto de la clase ImplanteMedico
+    def get_tipo(self):
+        return self.__tipo
+# se definen los getters y se   establecen las reglas de acceso a los atributos privados del objeto
+# se defines los setters para  poder asignarle valores a esos atributos privados desde fuera de la clase
+    def get_material(self):
+        return self.__material
+    
+    def get_tamaño(self):
+        return self.__tamaño
+
+    def get_fecha_implantacion(self):
+        return self.__fecha_implantacion
+
+    def set_fecha_implantacion(self, fecha):
+        self.__fecha_implantacion = fecha
+
+    def get_medico_responsable(self):
+        return self.__medico_responsable
+
+    def set_medico_responsable(self, medico):
+        self.__medico_responsable = medico
+
+    def get_estado(self):
+        return self.__estado
+
+    def set_estado(self, estado):
+        self.__estado = estado
